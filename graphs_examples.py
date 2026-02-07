9207f101d907119d32787fcbd34a11f791777bd3
Samba <samba.pendela@gmail.com>
2026-02-07 22:02:59 +0530

    matpltlib completed

--- graphs_examples.py
+++ graphs_examples.py
@@ -1,3 +1,70 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_excel("Book1.xlsx", engine="openpyxl")
+names = df["Name"]
+marks = df["Marks"]
+bars = plt.bar(names,marks,color="teal")
+for bar in bars:
+    plt.text(
+        bar.get_x() + bar.get_width()/2,
+        bar.get_height(),
+        bar.get_height(),
+        ha="center",
+        va="bottom"
+    )
+
+plt.title("Student Marks")
+plt.ylabel("Marks")
+plt.show()
+
+
+
+
+
+# import matplotlib.pyplot as plt
+# x = [1,2,3,4,5]
+# y = [10,20,25,30,40]
+# sizes = [100,200,300,400,1500]
+# colors = [10,20,30,40,50]
+# plt.scatter(x,y,s=sizes,c=colors,cmap="viridis")
+# plt.colorbar(label="Color Scale")
+# plt.xlabel("X")
+# plt.ylabel("Y")
+# plt.title("Advanced Scatter Plot")
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+# tasks = ["Task1","Task2","Task3","Task4"]
+# status = [90,75,60,70]
+# plt.barh(tasks,status,color="coral")
+# plt.xlabel("Completion")
+# plt.title("Progress Tracker")
+# plt.show()
+
+
+
+
+
+# import matplotlib.pyplot as plt
+# students = ["A","B","C","D"]
+# marks = [70,85,60,90]
+# bars = plt.bar(students,marks,color="orange")
+
+# for bar in bars:
+#     plt.text(
+#         bar.get_x() + bar.get_width()/2,
+#         bar.get_height(),
+#         bar.get_height(),
+#         ha="center",
+#         va="bottom"
+#     )
+
+# plt.title("Student Marks")
+# plt.ylabel("Marks")
+# plt.show()
+
+
 # import matplotlib.pyplot as plt
 # days = [1, 2, 3, 4, 5]
 # sales = [200, 400, 600, 800, 1000]
